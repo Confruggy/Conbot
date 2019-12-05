23bf2140556a877f562b3de8d68fceb5aad53b2b
Confruggy <confruggy@gmail.com>
2019-12-05 19:38:27 +0100

    config file support

---
+++ Config.cs
@@ -0,0 +1,9 @@
+namespace Conbot
+{
+    public class Config
+    {
+        public string Token { get; set; }
+        public string Secret { get; set; }
+        public int TotalShards { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Nett;
 
 namespace Conbot
 {
     class Program
     {
-        static void Main(string[] args)
+        private Config _config;
+
+        static Task Main() => new Program().StartAsync();
+
+        private async Task StartAsync()
         {
-            Console.WriteLine("Hello World!");
+            if (!ReadConfig())
+                return;
+
+            await Task.Delay(0);
+        }
+
+        private bool ReadConfig()
+        {
+            if (!File.Exists("config.toml"))
+            {
+                Console.WriteLine("Config file is missing. Please provide a config file!");
+                _config = new Config();
+                Toml.WriteFile(_config, "config.toml");
+            }
+            else
+            {
+                try
+                {
+                    _config = Toml.ReadFile<Config>("config.toml");
+                }
+                catch
+                {
+                    Console.WriteLine("Config file is invalid. Please provide a valid config file!");
+                    Console.ReadKey();
+                    return false;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(_config.Token))
+            {
+                Console.Write("Enter your bot token: ");
+                string token = Console.ReadLine();
+                _config.Token = token;
+                Toml.WriteFile(_config, "config.toml");
+            }
+            
+            return true;
         }
     }
 }
