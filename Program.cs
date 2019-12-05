18db9696e2fba3ef2adb53cef335339f67cbaa7d
Confruggy <confruggy@gmail.com>
2019-12-05 18:34:34 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
