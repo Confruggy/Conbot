b913f121ab61929650a00a984e34aff594e1cf86
Confruggy <confruggy@gmail.com>
2019-12-09 18:54:50 +0100

    Create DeletableExtensions.cs

---
+++ src/Extensions/DeletableExtensions.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Discord;
+using Discord.Net;
+
+namespace Conbot.Extensions
+{
+    public static class DeletableExtensions
+    {
+        public static async Task<bool> TryDeleteAsync(this IDeletable deletable)
+        {
+            try
+            {
+                await deletable.DeleteAsync().ConfigureAwait(false);
+                return true;
+            }
+            catch (HttpException)
+            {
+                return false;
+            }
+        }
+    }
+}
