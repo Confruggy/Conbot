b21559b2caec73c4792fe1dcd21037dfa0cbc05f
Confruggy <confruggy@gmail.com>
2019-12-10 00:16:07 +0100

    Create CommandContextExtensions.cs

---
+++ src/Extensions/CommandContextExtensions.cs
@@ -0,0 +1,48 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Conbot.Extensions
+{
+    public static class CommandContextExtensions
+    {
+        public static async Task<SocketUserMessage> WaitForMessageAsync(this SocketCommandContext context,
+            int timeout = 60000)
+        {
+            var tokenSource = new CancellationTokenSource();
+
+            var client = context.Client;
+
+            SocketUserMessage message = null;
+
+            Task func(SocketMessage msg)
+            {
+                if (msg.Channel.Id == context.Channel.Id && msg.Author.Id == context.User.Id &&
+                    msg is SocketUserMessage userMessage)
+                {
+                    message = userMessage;
+                    tokenSource.Cancel(true);
+                }
+                return Task.CompletedTask;
+            }
+
+            client.MessageReceived += func;
+
+            try
+            {
+                await Task.Delay(timeout, tokenSource.Token).ConfigureAwait(false);
+            }
+            catch
+            {
+                return message;
+            }
+            finally
+            {
+                client.MessageReceived -= func;
+            }
+
+            return null;
+        }
+    }
+}
