e5042aea31400f4f557e0ebec942c6c5fd32d8ae
Confruggy <confruggy@gmail.com>
2019-12-15 22:59:31 +0100

    Create BackgroundServiceStarter

---
+++ src/Conbot.Services/BackgroundServiceStarter.cs
@@ -0,0 +1,21 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Conbot.Services
+{
+    public class BackgroundServiceStarter<T> : IHostedService
+    where T : IHostedService
+    {
+        private readonly T _backgroundService;
+
+        public BackgroundServiceStarter(T backgroundService)
+            => _backgroundService = backgroundService;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+            => _backgroundService.StartAsync(cancellationToken);
+
+        public Task StopAsync(CancellationToken cancellationToken)
+            => _backgroundService.StopAsync(cancellationToken);
+    }
+}
