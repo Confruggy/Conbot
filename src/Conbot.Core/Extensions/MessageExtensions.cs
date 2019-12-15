963a5015172bfcd9f939053347b58b071567a71e
Confruggy <confruggy@gmail.com>
2019-12-15 16:27:42 +0100

    Add TryRemoveReactionAsync

--- src/Conbot.Core/Extensions/MessageExtensions.cs
+++ src/Conbot.Core/Extensions/MessageExtensions.cs
@@ -6,17 +6,59 @@ namespace Conbot.Extensions
 {
     public static class MessageExtensions
     {
-        public static async Task<bool> TryAddReactionAsync(this IUserMessage message, IEmote emote, RequestOptions options = null)
+        public static async Task<bool> TryAddReactionAsync(this IMessage message, IEmote emote,
+            RequestOptions options = null)
         {
             try
             {
                 await message.AddReactionAsync(emote, options).ConfigureAwait(false);
-                return true;
             }
             catch (HttpException)
             {
                 return false;
             }
+            return true;
+        }
+
+        public static async Task<bool> TryRemoveReactionAsync(this IMessage message, IEmote emote, ulong userId,
+            RequestOptions options = null)
+        {
+            try
+            {
+                await message.RemoveReactionAsync(emote, userId, options).ConfigureAwait(false);
+            }
+            catch (HttpException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static async Task<bool> TryRemoveReactionAsync(this IMessage message, IEmote emote, IUser user,
+            RequestOptions options = null)
+        {
+            try
+            {
+                await message.RemoveReactionAsync(emote, user, options).ConfigureAwait(false);
+            }
+            catch (HttpException)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static async Task<bool> TryRemoveAllReactionsAsync(this IMessage message, RequestOptions options = null)
+        {
+            try
+            {
+                await message.RemoveAllReactionsAsync(options).ConfigureAwait(false);
+            }
+            catch (HttpException)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
