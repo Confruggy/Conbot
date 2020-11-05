8948066e6c944e378c32321e5e78ad6094ed9a0a
Confruggy <confruggy@gmail.com>
2020-11-05 20:48:18 +0100

    Add ContextExtensions

---
+++ src/Plugins/Conbot.TimeZonePlugin/Extensions/ContextExtensions.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using Conbot.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using NodaTime;
+
+namespace Conbot.TimeZonePlugin.Extensions
+{
+    public static class ContextExtensions
+    {
+        public static async ValueTask<DateTimeZone> GetUserTimeZoneAsync(this DiscordCommandContext context)
+        {
+            var db = context.ServiceProvider.GetRequiredService<TimeZoneContext>();
+            var provider = context.ServiceProvider.GetRequiredService<IDateTimeZoneProvider>();
+
+            var userTimeZone = await db.GetUserTimeZoneAsync(context.User);
+
+            return userTimeZone != null ? provider.GetZoneOrNull(userTimeZone.TimeZoneId) : null;  
+        }
+
+        public static async ValueTask<DateTimeZone> GetGuildTimeZoneAsync(this DiscordCommandContext context)
+        {
+            if (context.Guild == null)
+                return null;
+
+            var db = context.ServiceProvider.GetRequiredService<TimeZoneContext>();
+            var provider = context.ServiceProvider.GetRequiredService<IDateTimeZoneProvider>();
+
+            var guildTimeZone = await db.GetGuildTimeZoneAsync(context.Guild);
+
+            return guildTimeZone != null ? provider.GetZoneOrNull(guildTimeZone.TimeZoneId) : null;  
+        }
+    }
+}
