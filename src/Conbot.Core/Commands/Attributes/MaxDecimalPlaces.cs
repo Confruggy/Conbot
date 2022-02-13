9eb8a29802efe5a510edbb1192bbbb6351c00ff4
Confruggy <confruggy@gmail.com>
2022-02-13 20:03:30 +0100

    Add MaxDecimalPlacesAttribute

---
+++ src/Conbot.Core/Commands/Attributes/MaxDecimalPlaces.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+using Humanizer;
+
+using Qmmands;
+
+namespace Conbot.Commands;
+
+public class MaxDecimalPlacesAttribute : ParameterCheckAttribute
+{
+    public int MaxDecimalPlaces { get; set; }
+
+    public MaxDecimalPlacesAttribute(int maxDecimalPlaces) => MaxDecimalPlaces = maxDecimalPlaces;
+
+    public override ValueTask<CheckResult> CheckAsync(object argument, CommandContext context)
+    {
+        if (argument is not decimal value)
+            return CheckResult.Failed("This check must be used on a decimal parameter.");
+
+        string text = value.ToString(CultureInfo.InvariantCulture);
+
+        int decimalPlaces = text.Contains(".")
+            ? text[(text.IndexOf(".", StringComparison.Ordinal) + 1)..].Length
+            : 0;
+
+        return decimalPlaces > MaxDecimalPlaces
+            ? CheckResult.Failed($"{Parameter.Name.Humanize()} can't have more than {MaxDecimalPlaces} decimal places.")
+            : CheckResult.Successful;
+    }
+}
